b9d91740195e07134f9bc6152019f10f3a3984d7
Chayan <chayangope12@gmail.com>
2024-03-24 09:30:05 +0530

    Committed

---
+++ file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function to calculate the factorial of a number
+unsigned long long factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    // Check if the user has entered a negative number
+    if (number < 0)
+        printf("Factorial of a negative number doesn't exist.\n");
+    else
+        printf("Factorial of %d = %llu\n", number, factorial(number));
+
+    return 0;
+}
+
